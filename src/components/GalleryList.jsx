dc75cbaf90292280d1aac03f4a97413c3bd3a422
doooox <dtopic12@gmail.com>
2022-12-12 17:06:23 +0100

    Add all Galleries, userGallery, view gallery with pagination and filter

--- src/components/GalleryList.jsx
+++ src/components/GalleryList.jsx
@@ -1,56 +1,57 @@
 import React from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { Link } from "react-router-dom";
+import { selectIsHidden } from "../store/gallery/selectors";
 import { setCurrentPage } from "../store/gallery/slice";
-
-const GalleriesList = ({ galleries }) => {
-    const dispatch = useDispatch();
-    return (
-        <div className='p-3 text-center'>
-            {galleries.length ? (
-                galleries.map((gallery, index) => (
-                    <div key={index}>
-                        <h1>
-                            <Link className='text-dark' to={`${gallery.id}`}>
-                                {gallery.name}
-                            </Link>
-                        </h1>
-                        <a target="_blank" href={gallery?.images[0]?.image_url} >
-                            <img
-                                className='rounded mx-auto'
-                                style={{
-                                    width: "300px",
-                                    height: "200px",
-                                }}
-                                key={gallery?.images[0]?.id}
-                                src={gallery?.images[0]?.image_url}
-                                alt="Image" />
-                        </a>
-                        <p style={{ fontSize: "1.5em" }}>{gallery.description}</p>
-                        <span>
-                            Created by{" "}
-                            <Link className='text-dark' to={`/autors/${gallery.user_id}`}>
-                                {gallery.user.first_name} {gallery.user.last_name}
-                            </Link>{" "}
-                            <em>{Date(gallery.created_at).toLocaleString()}</em>
-                        </span>
-                        <hr />
-                    </div>
-                ))
-            ) : (
-                <div className='display-4'>No galleries have been created</div>
-            )}
-            {galleries.length % 10 === 0 ? <button
-                className='btn'
-                type='button'
-                onClick={() => dispatch(setCurrentPage())}
-            >
-                Load More
-            </button> :
-                null
-            }
-        </div>
-    );
+function GalleriesList({ galleries }) {
+  const dispatch = useDispatch();
+  const isHidden = useSelector(selectIsHidden);
+  return (
+    <div className=' m-3 text-center'>
+      {galleries.length ? (
+        galleries.map((gallery, index) => (
+          <div key={index}>
+            <h1>
+              <Link className='text-dark' to={`galleries/${gallery.id}`}>
+                {gallery.name}
+              </Link>
+            </h1>
+            <img
+              className='rounded mx-auto d-block'
+              style={{
+                width: "300px",
+                height: "200px",
+              }}
+              src={
+                gallery.images[0]?.image_url
+              }
+              alt='picture'
+            />
+            <p style={{ fontSize: "1.5em" }}>{gallery.description}</p>
+            <span>
+              Created by{" "}
+              <Link className='text-dark' to={`/authors/${gallery.user_id}`}>
+                {gallery.user.first_name} {gallery.user.last_name}
+              </Link>{" "}
+              <em>{gallery.created_at}</em>
+            </span>
+            <hr />
+          </div>
+        ))
+      ) : (
+        <div className='display-4'>No galleries have been created</div>
+      )}
+      {!isHidden && (
+        <button
+          className='btn'
+          type='button'
+          onClick={() => dispatch(setCurrentPage())}
+        >
+          Load More
+        </button>
+      )}
+    </div>
+  );
 }
 
 export default GalleriesList;
--- src/store/gallery/selectors.js
+++ src/store/gallery/selectors.js
@@ -1,6 +1,6 @@
-export const selectGalleries = (state) => {
-  let galleries = state.gallery.galleries;
-  return galleries;
-};
+export const selectGalleries = (state) => state.gallery.galleries;
 export const selectCurrentPage = (state) => state.gallery.currentPage;
+export const selectIsHidden = (state) => state.gallery.isHidden;
 export const selectSearch = (state) => state.gallery.search;
+export const selectGallery = (state) => state.gallery.gallery;
+export const selectCurrentPicture = (state) => state.gallery.currentPicture;
